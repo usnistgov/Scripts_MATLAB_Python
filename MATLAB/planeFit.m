046a60b47b83005fa4425134b01344c086c8294d
Prem Rachakonda <prem.rachakonda@nist.gov>
2020-03-10 17:40:45 -0400

    Using EIG instead of SVD

--- MATLAB/planeFit.m
+++ MATLAB/planeFit.m
@@ -1,12 +1,24 @@
-function result = planeFit3(data)
+function result = planeFit2(data)
+% result = planeFitT(data)
+% data has to be of the form N rows, 3 columns for this to work. 
+% 
+% Prem Rachakonda 
+
+%Translate the data to its centroid
 centroid = mean(data,1);
 dataC = bsxfun(@minus,data,centroid);
-[U,S,V] = svd(dataC,0);
-P = V(:,3);
+
+%Calculate the eigen vectors and obtain the normal vector
+[V,~] = eig(dataC'*dataC);
+P = V(:,1);
+
 P = P/sqrt(sum(P.*P));
-d = -dot(P,cm);
+d = -dot(P,centroid);
 P(4) = d;
 
 P = P*sign(P(1)); %Just making sure that the sign of the equations are always the same
 a = P(1); b = P(2); c= P(3); d = P(4);
+xx= data(:,1); yy = data(:,2); zz = data(:,3);
+
 result.Parameters = [a b c d];
+result.Residuals = (a*xx + b*yy + c*zz + d)/sqrt(a^2 + b^2 + c^2);
